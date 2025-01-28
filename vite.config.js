0f14a90edd4e07ca5efd58461f2fd66efa850742
yashah9 <yashshah0910@gmail.com>
2025-01-28 18:59:36 -0500

    2143

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // IMPORTANT: Use your repo name
+  base: '/yashah9.github.io/', // IMPORTANT: Use your repo name
 });
