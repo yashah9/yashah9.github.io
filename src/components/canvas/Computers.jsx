7b91e0a19517cc6371549c9835c5a4307e28ac66
yashah9 <yashshah0910@gmail.com>
2025-01-31 18:38:27 -0500

    fiinally DONE

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,16 +3,17 @@ import React from "react";
 const ComputersCanvas = () => {
   return (
     <div className="relative flex justify-center items-center min-h-screen w-full pb-10 sm:pb-20">
-      {/* Yash Image (Moved Up) */}
+      {/* Yash Image (Properly Centered) */}
       <img 
         src="/Yash.JPG"  // ✅ Direct Public Path
         alt="Yash" 
-        className="absolute top-1/2 transform -translate-y-1/3 w-[2000px] h-[300px] 
-                   sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[450px] 
-                   lg:w-[2000px] lg:h-[500px] object-contain rounded-lg"
+        className="absolute top-1/2 transform -translate-y-1/2
+                   w-[200px] h-[250px] sm:w-[300px] sm:h-[350px] 
+                   md:w-[350px] md:h-[400px] lg:w-[400px] lg:h-[450px] 
+                   max-w-full object-contain rounded-lg"
       />
     </div>
   );
-};  
+};
 
 export default React.memo(ComputersCanvas);
